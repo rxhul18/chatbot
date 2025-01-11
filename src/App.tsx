9ec54900217785cc8574f06c4760c896e792cbec
rxhul18 <rahulshah0728@gmail.com>
2025-01-11 21:22:13 +0530

    toggle added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import "./App.css";
 import ChatBot from './components/custom';
+import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from "./components/custom/navbar";
 
 export default function App() {
     return (
-        <>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Navbar />
             <div className="bg-secondary/30 flex justify-center h-screen">
                 <div className='p-10 md:p-24 flex flex-col items-center gap-5 container mx-auto'>
                     <h1 className='text-5xl md:text-6xl font-semibold tracking-tighter drop-shadow-sm select-none'>This is how the chatbot looks like.</h1>
                     <ChatBot
-                        chatBotWrapperStyle="absolute bottom-4 right-4 md:top-58 max-h-[600px] shadow-xl"
+                        chatBotWrapperStyle="fixed bottom-4 right-4 md:top-58 max-h-[600px]"
                         chatWindowStyle="bg-black"
                         botIcon=""
                         userIcon=""
-                        // prompt="You are an artist" 
                         startOpen={true}
                         authToken={""}
                     />
                 </div>
             </div>
-        </>
-
+        </ThemeProvider>
     )
 }
 
